94cb7e18ff0d7886d3030fe6d18ee4b48495ca0f
山本 浩司 <k.yamamoto03@iforcom.jp>
2024-11-19 17:21:31 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorDraw/LineData.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorDraw;
+
+internal class LineData
+{
+    public double x1 { get; set; }
+    public double y1 { get; set; }
+    public double x2 { get; set; }
+    public double y2 { get; set; }
+
+    public bool IsDragging { get; set; } = false;
+}
